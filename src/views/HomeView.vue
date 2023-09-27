e61d09cbc006e1d25d30e4b889a1afe1d39e51cf
Moronfolu Olufunke <moronfolu.motunrayo1@gmail.com>
2023-09-27 01:29:43 +0100

    feat: creating the landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,25 @@
 <template>
-	<main>Hello World!</main>
+	<header class="vh-100 bg-light-pink dt w-100 helvetica">
+		<div class="background-container dtc v-mid cover ph3 ph4-m ph5-l">
+			<div class="flex justify-around">
+				<div>
+					<h1 class="f2 f-subheadline-l measure lh-title nb1 fw9 white">
+						Xata File Explorer
+					</h1>
+					<h2 class="f6 fw6 white">
+						Storing and managing files made easy with Xata (File Attachments)
+					</h2>
+				</div>
+			</div>
+		</div>
+	</header>
 </template>
+
+<style>
+	.background-container {
+		background-image: url("../assets/tamanna-rumee-QR56LDZADZ4-unsplash.jpg");
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+</style>
